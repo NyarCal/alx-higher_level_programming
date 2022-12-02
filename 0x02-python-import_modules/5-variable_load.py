ca4cb62d4423fb4a86815ebfc45bd1ae432188bd
NYARCAL <calvinnyakwe@gmail.com>
2022-12-02 06:25:03 -0800

    load variables

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 5-variable_load.py
+if _name_ == "_main_":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+    print(a)
+    
