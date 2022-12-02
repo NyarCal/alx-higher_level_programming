23d8d6b8d8a229123d72c02c9e6cb006e98dcedf
NYARCAL <calvinnyakwe@gmail.com>
2022-12-02 09:18:31 -0800

    ASCII ALPHABET

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for letter in range(97, 123):
+for alphabet in range(97, 123):
     if alphabet == 101 or alphabet == 113:
         continue
         print("{:c}".format(alphabet), end='')
