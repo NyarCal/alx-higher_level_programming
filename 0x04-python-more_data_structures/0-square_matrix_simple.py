7de053eb683aae7a1bf08606630d12c41f3eb38d
NYARCAL <calvinnyakwe@gmail.com>
2022-12-09 05:49:30 -0800

    update square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    
     # Create a new matrix with the same size as the input matrix
-    
     new_matrix = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
-    
     # Iterate over the elements in the matrix
-    
     for i in range(len(matrix)):
-        
         for j in range(len(matrix[i])):
-            
             # Square the element and store it in the new matrix
-            
-            new_matrix[i][j] = matrix[i][j] ** 2
-            
-    return new_matrix
+             new_matrix[i][j] = matrix[i][j] ** 2
+     return new_matrix
 
